46003b33a7d8333f6cb63502afad979fd5e640de
Yairzdr <zederman@post.bgu.ac.il>
2020-11-01 22:54:45 +0200

    ver0.0

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -3,5 +3,26 @@
 //
 
 #include "../include/Graph.h"
+// simple constructor
+//Graph::Graph(std::vector<std::vector<int>> matrix):edges(matrix){}
+
+// destructor
+
+// copy constructor
+
+// copy assignment operator
+
+// move constructor
+
+// move assignment operator
+
+void Graph::infectNode(int nodeInd) {
+    // this function infects a node
+}
+
+bool Graph::isInfected(int nodeInd) {
+    // this function checks if the given node is infected
+    return false;
+}
 
 
